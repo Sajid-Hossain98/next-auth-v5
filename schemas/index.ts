d86d0266c26ef745b547e9e030d904a0e786ee67
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-10 14:04:28 +0600

    schema for register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,15 @@ export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string(),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+  name: z.string().min(3, {
+    message: "Name is required.",
+  }),
+});
