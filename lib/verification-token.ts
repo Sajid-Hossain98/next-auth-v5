efcfae11b1a6acb723b732074350a4ccef8409c4
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-16 16:15:40 +0600

    helper function to get verification token by email

--- lib/verification-token.ts
+++ lib/verification-token.ts
@@ -13,3 +13,17 @@ export const getVerificationTokenByToken = async (token: string) => {
     return null;
   }
 };
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    return verificationToken;
+  } catch {
+    return null;
+  }
+};
