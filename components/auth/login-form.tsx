8156a5e6fd06efafef7a5bddac57e709c9b4e8d1
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-16 16:03:43 +0600

    showing auth error in the login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -19,11 +19,20 @@ import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email is already in use with different provider!"
+      : "";
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
+
+  console.log(urlError);
 
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
@@ -39,7 +48,9 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error), setSuccess(data.success);
+        setError(data?.error);
+        //TODO: add during 2FA
+        //  setSuccess(data.success);
       });
     });
   };
@@ -99,7 +110,7 @@ export const LoginForm = () => {
             />
           </div>
 
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
 
           <Button className="w-full" type="submit" disabled={isPending}>
