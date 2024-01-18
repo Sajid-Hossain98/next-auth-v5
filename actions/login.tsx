bdcfd308b56698cf97cdbdfb9c2e42f3cca692e1
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-18 19:27:32 +0600

    sending verification confirm email for register and login

--- actions/login.tsx
+++ actions/login.tsx
@@ -6,6 +6,7 @@ import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -27,6 +28,11 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   if (!existingUser.emailVerified) {
     const verificationToken = await generateVerificationToken(
       existingUser.email
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
     );
 
     return {
--- actions/register.tsx
+++ actions/register.tsx
@@ -6,6 +6,7 @@ import * as z from "zod";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -35,7 +36,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const verificationToken = await generateVerificationToken(email);
 
-  // TODO: send verification token email
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return {
     success: "Confirmation email sent!",
