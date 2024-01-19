6d57f60d84507dfe759b9f6c0948eb3b4c2fba6d
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-19 20:44:00 +0600

    onSubmit functionality for token verification form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -3,15 +3,31 @@
 import { BeatLoader } from "react-spinners";
 import { CardWrapper } from "./card-wrapper";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormSuccess } from "../form-success";
+import { FormError } from "../form-error";
 
 export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("Missing token");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success), setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -25,7 +41,10 @@ export const NewVerificationForm = () => {
       backButtonLabel="Back to login"
     >
       <div className="flex items-center justify-center w-full">
-        <BeatLoader />
+        {!success && !error && <BeatLoader />}
+
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
