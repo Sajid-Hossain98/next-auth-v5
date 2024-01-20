a1c07aaee6101f8121b36332554115ea3ac98148
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-20 22:03:02 +0600

    Page for resetting the password

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -20,6 +20,7 @@ import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export const LoginForm = () => {
   const searchParams = useSearchParams();
@@ -102,6 +103,15 @@ export const LoginForm = () => {
                       disabled={isPending}
                     />
                   </FormControl>
+
+                  <Button
+                    size="sm"
+                    variant="link"
+                    className="px-0 font-normal"
+                    asChild
+                  >
+                    <Link href="/auth/reset">Forgot password?</Link>
+                  </Button>
 
                   <FormMessage />
                 </FormItem>
