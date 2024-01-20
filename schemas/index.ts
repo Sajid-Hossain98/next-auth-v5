19edde0b876804204237e0026ef577e719b54f04
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-20 22:16:46 +0600

    password resetting schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,18 +1,28 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Password must contain at lease 6 characters",
+  }),
 });
 
 export const RegisterSchema = z.object({
   email: z.string().email({
-    message: "Email is required.",
+    message: "Email is required",
   }),
   password: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
+    message: "Password must be at least 6 characters",
   }),
   name: z.string().min(3, {
     message: "Name is required.",
+  }),
+});
+
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
   }),
 });
