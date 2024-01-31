f78d8a51ad1e8810e6cb89473098950c5f6fd883
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-01-31 21:21:51 +0600

    client page with user info

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -50,13 +50,9 @@ export const UserInfo = ({ user, label }: UserInfoProps) => {
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Two Factor Authentication</p>
 
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
-            <Badge
-              variant={user?.isTwoFactorEnabled ? "success" : "destructive"}
-            >
-              {user?.isTwoFactorEnabled ? "ON" : "OFF"}
-            </Badge>
-          </p>
+          <Badge variant={user?.isTwoFactorEnabled ? "success" : "destructive"}>
+            {user?.isTwoFactorEnabled ? "ON" : "OFF"}
+          </Badge>
         </div>
       </CardContent>
     </Card>
