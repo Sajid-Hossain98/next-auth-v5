3b0b02b77dbf6c6caa8cf3b7ce2ab32ad8e03164
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-01 21:12:34 +0600

    admin page with role-gate component for role based contents

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,9 +1,27 @@
-import { currentUserRole } from "@/lib/auth";
+"use client";
 
-const AdminPage = async () => {
-  const role = currentUserRole();
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useCurrentRole } from "@/hooks/useCurrentRole";
+import { UserRole } from "@prisma/client";
 
-  return <div>Current Role: {role}</div>;
+const AdminPage = () => {
+  const role = useCurrentRole();
+
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🗝️ Admin</p>
+      </CardHeader>
+
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to see this content!" />
+        </RoleGate>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
