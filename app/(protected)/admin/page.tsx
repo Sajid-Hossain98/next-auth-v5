561987e9533baff661a22581b926c019afe6c648
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-01 22:20:23 +0600

    content in admin page for Admin only

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -2,12 +2,24 @@
 
 import { RoleGate } from "@/components/auth/role-gate";
 import { FormSuccess } from "@/components/form-success";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useCurrentRole } from "@/hooks/useCurrentRole";
 import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
 
 const AdminPage = () => {
   const role = useCurrentRole();
+
+  const onApiRouteClick = () => {
+    fetch("/api/admin").then((response) => {
+      if (response.ok) {
+        toast.success("Allowed API Route!");
+      } else {
+        toast.error("FORBIDDEN API Route!");
+      }
+    });
+  };
 
   return (
     <Card className="w-[600px]">
@@ -19,6 +31,18 @@ const AdminPage = () => {
         <RoleGate allowedRole={UserRole.ADMIN}>
           <FormSuccess message="You are allowed to see this content!" />
         </RoleGate>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only API Route</p>
+
+          <Button onClick={onApiRouteClick}>Click to test</Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+
+          <Button>Click to test</Button>
+        </div>
       </CardContent>
     </Card>
   );
