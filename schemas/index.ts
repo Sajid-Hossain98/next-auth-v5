388734257814655389d28ba13df3adfa5e212de6
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-02 22:56:36 +0600

    settings initial schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -33,3 +33,7 @@ export const NewPasswordSchema = z.object({
     message: "Password must be at least 6 characters",
   }),
 });
+
+export const SettingsSchema = z.object({
+  name: z.optional(z.string()),
+});
