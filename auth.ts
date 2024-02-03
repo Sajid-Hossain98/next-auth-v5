fa117472ae30e5dc6d0c89e8174760afe421e2ec
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-03 22:14:50 +0600

    changes for settings update in auth.ts

--- auth.ts
+++ auth.ts
@@ -70,6 +70,11 @@ export const {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -79,6 +84,8 @@ export const {
 
       if (!existingUser) return token;
 
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
