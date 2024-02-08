30db6669aa8d0a5df48d15ded4afc101aea1d7c7
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-08 22:12:04 +0600

    if the user is an OAuth user then can't change some settings

--- actions/settings.ts
+++ actions/settings.ts
@@ -23,6 +23,13 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     };
   }
 
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
   await db.user.update({
     where: {
       id: dbUser.id,
