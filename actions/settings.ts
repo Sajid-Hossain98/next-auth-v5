552586d69bca41607b7b4f5e0252b988b263fa61
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-02-09 22:30:48 +0600

    server action for updating password

--- actions/settings.ts
+++ actions/settings.ts
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 import { getUserByEmail, getUserById } from "@/data/user";
 import { generateVerificationToken } from "@/lib/tokens";
 import { sendVerificationEmail } from "@/lib/mail";
+import bcrypt from "bcryptjs";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -50,6 +51,23 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     return {
       success: "Verification email sent!",
     };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password
+    );
+
+    if (!passwordMatch) {
+      return {
+        error: "Incorrect password!",
+      };
+    }
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
   }
 
   await db.user.update({
